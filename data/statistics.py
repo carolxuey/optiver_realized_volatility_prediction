af4662f5049dbb0d64037cef7d8d3efb21554826
gunesevitan <gunesevitan@gmail.com>
2021-07-26 19:17:49 +0300

    statistics with new features

--- data/statistics.py
+++ data/statistics.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
@@ -24,11 +25,18 @@ def get_book_statistics(df):
     book_features = [
         'bid_price1', 'ask_price1', 'bid_price2', 'ask_price2',
         'bid_size1', 'ask_size1', 'bid_size2', 'ask_size2',
+        'wap1', 'wap2', 'wap1_squared_log_returns', 'wap2_squared_log_returns'
     ]
     df_books = pd.DataFrame(columns=book_features)
 
     for stock_id in tqdm(sorted(df['stock_id'].unique())):
         df_book = preprocessing_utils.read_book_data('train', stock_id, sort=True, forward_fill=True)
+        df_book['wap1'] = (df_book['bid_price1'] * df_book['ask_size1'] + df_book['ask_price1'] * df_book['bid_size1']) /\
+                          (df_book['bid_size1'] + df_book['ask_size1'])
+        df_book['wap2'] = (df_book['bid_price2'] * df_book['ask_size2'] + df_book['ask_price2'] * df_book['bid_size2']) /\
+                          (df_book['bid_size2'] + df_book['ask_size2'])
+        df_book['wap1_squared_log_returns'] = np.log(df_book['wap1'] / df_book.groupby('time_id')['wap1'].shift(1)) ** 2
+        df_book['wap2_squared_log_returns'] = np.log(df_book['wap2'] / df_book.groupby('time_id')['wap2'].shift(1)) ** 2
         df_books = pd.concat([df_books, df_book.loc[:, book_features]], axis=0, ignore_index=True)
 
     means = df_books.mean(axis=0).to_dict()
@@ -51,11 +59,12 @@ def get_trade_statistics(df):
     stds (dict): Stds of specified features on entire training set
     """
 
-    trade_features = ['price', 'size', 'order_count']
+    trade_features = ['price', 'size', 'order_count', 'price_squared_log_returns']
     df_trades = pd.DataFrame(columns=trade_features)
 
     for stock_id in tqdm(sorted(df['stock_id'].unique())):
         df_trade = preprocessing_utils.read_trade_data(df, 'train', stock_id, sort=True, zero_fill=True)
+        df_trade['price_squared_log_returns'] = np.log(df_trade['price'] / df_trade.groupby('time_id')['price'].shift(1)) ** 2
         df_trades = pd.concat([df_trades, df_trade.loc[:, trade_features]], axis=0, ignore_index=True)
 
     means = df_trades.mean(axis=0).to_dict()

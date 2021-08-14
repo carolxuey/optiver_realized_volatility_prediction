772ea05270d16e3cbb7fcb6b84b11b7eb4a5f9a0
gunesevitan <gunesevitan@gmail.com>
2021-08-14 15:20:28 +0300

    updates

--- src/rnn_model.py
+++ src/rnn_model.py
@@ -14,31 +14,28 @@ class RNNRegularModel(nn.Module):
         self.num_layers = num_layers
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-        self.stock_embeddings = nn.Embedding(num_embeddings=113, embedding_dim=16)
-        self.batch_norm = nn.BatchNorm1d(input_size)
+        self.batch_norm = nn.BatchNorm1d(600)
         self.lstm = nn.LSTM(
             input_size=self.input_size,
             hidden_size=self.hidden_size,
             num_layers=self.num_layers,
-            dropout=0,
+            dropout=0.25,
             bidirectional=False,
             batch_first=True
         )
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.25)
         self.head = nn.Sequential(
-            nn.Linear(hidden_size + 16, 1, bias=True),
+            nn.Linear(hidden_size, 1, bias=True),
             SigmoidRange(0, 0.1)
         )
 
-    def forward(self, stock_ids, sequences):
+    def forward(self, sequences):
 
-        x = torch.transpose(sequences, 1, 2)
-        x = self.batch_norm(x)
-        h_n0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-        c_n0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-        lstm_output, (h_n, c_n) = self.lstm(x, (h_n0, c_n0))
-        avg_pooled_features = torch.mean(lstm_output, 1)
-        embedded_stock_ids = self.stock_embeddings(stock_ids)
-        x = torch.cat([avg_pooled_features, self.dropout(embedded_stock_ids)], dim=1)
-        output = self.head(x)
+        self.sequences = self.batch_norm(sequences)
+        h_n0 = torch.zeros(self.num_layers, sequences.size(0), self.hidden_size).to(self.device)
+        c_n0 = torch.zeros(self.num_layers, sequences.size(0), self.hidden_size).to(self.device)
+        lstm_output, (h_n, c_n) = self.lstm(sequences, (h_n0, c_n0))
+        output = self.head(h_n.view(-1, self.hidden_size))
 
         return output

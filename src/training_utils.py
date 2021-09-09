466633421c59e67c0e8ae5f6d822afb948b3374e
gunesevitan <gunesevitan@gmail.com>
2021-09-09 22:35:27 +0300

    custom eval and loss functions for lightgbm model

--- src/training_utils.py
+++ src/training_utils.py
@@ -63,3 +63,50 @@ def rmspe_loss_pt(y_true, y_pred):
 
     rmspe = torch.sqrt(torch.mean(torch.square((y_true - y_pred) / y_true)))
     return rmspe
+
+
+def rmspe_eval_lgb(y_pred, train_dataset):
+
+    """
+    Calculate root mean squared percentage error between ground-truth and predictions
+
+    Parameters
+    ----------
+    y_pred [array-like of shape (n_samples)]: Predictions
+    train_dataset (lightgbm.basic.Dataset): Training dataset
+
+    Returns
+    -------
+    eval_name (str): Name of the evaluation metric
+    eval_result (float): Result of the evaluation metric
+    is_higher_better (bool): Whether the higher is better or worse for the evaluation metric
+    """
+
+    eval_name = 'rmspe'
+    y_true = train_dataset.get_label()
+    eval_result = np.sqrt(np.mean(np.square((y_true - y_pred) / y_true)))
+    is_higher_better = False
+
+    return eval_name, eval_result, is_higher_better
+
+
+def rmspe_loss_lgb(y_pred, train_dataset):
+
+    """
+    Calculate gradient and hessian of the loss function root mean squared percentage error
+
+    Parameters
+    ----------
+    y_pred [array-like of shape (n_samples)]: Predictions
+    train_dataset (lightgbm.basic.Dataset): Training dataset
+
+    Returns
+    -------
+    gradient (float): First order derivative of the loss function root mean squared percentage error
+    hessian (float): Second order derivative of the loss function root mean squared percentage error
+    """
+
+    y_true = train_dataset.get_label()
+    gradient = 2.0 / y_true * (y_pred * 1.0 / y_true - 1)
+    hessian = 2.0 / (y_true ** 2)
+    return gradient, hessian
